f4e5196f22a37375637be9280ec3b18e3873f6e0
matthewlapeer <64608882+matthewlapeer@users.noreply.github.com>
2023-09-09 15:43:12 -0400

    update configs

--- svelte.config.js
+++ svelte.config.js
@@ -4,17 +4,6 @@ import adapter from '@sveltejs/adapter-auto';
 const config = {
 	kit: {
 		adapter: adapter(),
-		vite: {
-			optimizeDeps: {
-				include: ["highlight.js", "highlight.js/lib/core"],
-			},
-			server: {
-				fs: {
-				// Allow serving files from one level up to the project root
-					allow: ['..'],
-				},
-			},
-		}
 	}
 };
 
